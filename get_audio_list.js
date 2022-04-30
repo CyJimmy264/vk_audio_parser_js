d39a2246dd59f79b56576174c77cf4724e1e9b6e
mveynberg <mv@cj264.ru>
2022-04-30 11:59:19 +0500

    Save XSPF

--- get_audio_list.js
+++ get_audio_list.js
@@ -1,4 +1,34 @@
 /*
+  console.save_xspf
+*/
+(function (console) {
+
+  console.save_xspf = function (data, filename) {
+
+    if (!data) {
+      console.error('Console.save: No data')
+      return;
+    }
+
+    if (!filename) filename = 'console.json'
+
+    if (typeof data === "object") {
+      data = JSON.stringify(data, undefined, 4)
+    }
+
+    var blob = new Blob([data], { type: 'application/xspf' }),
+      e = document.createEvent('MouseEvents'),
+      a = document.createElement('a')
+
+    a.download = filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl = ['application/xspf', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+  }
+})(console)
+
+/*
   VK Audio Url Decode
 */
 var skip_scrolling = true;
@@ -238,3 +268,5 @@ for (const track_list_chunk of track_list_chunks) {
 xspf += `  </trackList>
 </playlist>
 `
+
+console.save_xspf(xspf, 'prepend_playlist.xspf')
